5d03b0820277f118c75a30d8cc2579ce92a8a35e
Vasu Khandelwal <vasu241057@gmail.com>
2023-01-05 12:55:32 +0530

    Updates

---
+++ precache-manifest.b7a840eaeb41b7740287ef968725701f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0320e0b96aa54be80761b3ef4f55ee59",
+    "url": "/recipeApp/index.html"
+  },
+  {
+    "revision": "132dd910d303d01c77a4",
+    "url": "/recipeApp/static/css/main.a35afe32.chunk.css"
+  },
+  {
+    "revision": "7836e72e88ff5660f410",
+    "url": "/recipeApp/static/js/2.abde64f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipeApp/static/js/2.abde64f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "132dd910d303d01c77a4",
+    "url": "/recipeApp/static/js/main.87b0e1c5.chunk.js"
+  },
+  {
+    "revision": "f146406a09ff9ab510db",
+    "url": "/recipeApp/static/js/runtime-main.38c2d46b.js"
+  },
+  {
+    "revision": "536ce0ac077c39cdaeadb46099046f51",
+    "url": "/recipeApp/static/media/bg-2.536ce0ac.jpg"
+  },
+  {
+    "revision": "3e8e998a323a82c5833b7eca555b946c",
+    "url": "/recipeApp/static/media/unavailable.3e8e998a.png"
+  }
+]);
